88f97fd01d2a12f7383e2c81d9793d3b2315c22a
Vadim Shikhovtsov <5208643@mail.ru>
2025-03-31 16:35:28 +0300

    Add day1 task1

---
+++ Practice/task1/main.go
@@ -0,0 +1,15 @@
+package main
+
+
+// TODO. Реализовать REST API CRUD для коллекции "Книги"
+
+type Book struct{
+	Title		string
+	Author		string
+	ISBN		string
+	Publisher	string
+	Year		int
+	//...
+}
+
+func main(){}
