f50da8f4866d58c9c8e6dc39ec2868b24fa9c3f4
Vadim Shikhovtsov <5208643@mail.ru>
2025-04-01 16:22:06 +0300

    Add CreatePost and GetPost

--- go_graphql/database/database.go
+++ go_graphql/database/database.go
@@ -2,12 +2,14 @@ package database
 
 import (
 	"context"
+	"go_graphql/graph/model"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
-	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 
@@ -42,4 +44,50 @@ func Connect() *DB {
 
 func collectionHelper(db *DB, collectionName string) *mongo.Collection {
 	return db.client.Database("blog_posts").Collection(collectionName)
+}
+
+func (db *DB) GetPost(id string) *model.Post {
+	collection := collectionHelper(db, "posts")
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+
+	Id, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": Id}
+
+	var post model.Post
+
+	err = collection.FindOne(ctx, filter).Decode(&post)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &post
+}
+
+func (db *DB) CreatePost(postInfo *model.NewPost) *model.Post {
+	collection := collectionHelper(db, "posts")
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+
+	postInfo.PublishedAt = timePtr(time.Now())
+	postInfo.UpdatedAt = timePtr(time.Now())
+
+	result, err := collection.InsertOne(ctx, postInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newPost := &model.Post{
+		ID: result.InsertedID.(bson.ObjectID).Hex(),
+		Title: postInfo.Title,
+		Content: postInfo.Content,
+		Author: *postInfo.Author,
+		Hero: *postInfo.Hero,
+		PublishedAt: *postInfo.PublishedAt,
+		UpdatedAt: *postInfo.UpdatedAt,
+	}
+	return newPost
 }
